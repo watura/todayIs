fdb54d48ec024cde9f0442eeebf85d46175ea4b1
NISHIMOTO Wataru <watura@g91.org>
2012-01-30 15:25:19 +0900

    Initial Commit

---
+++ todayIs/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  todayIs
+//
+//  Created by Wataru Nishimoto on 12/01/30.
+//  Copyright (c) 2012年 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ todayIs/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  todayIs
+//
+//  Created by Wataru Nishimoto on 12/01/30.
+//  Copyright (c) 2012年 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
